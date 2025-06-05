a9d453ee40465edb9b6a8bd22127dbac9cdf6adb
Ryan20040419 <1072511002@ulife.com.br>
2025-06-05 20:12:18 -0300

    Add files via upload
    MAIN FEITO

---
+++ Main.java
@@ -0,0 +1,7 @@
+import EstoqueCrypto.ProdutoDAO;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new FrontLogin().setVisible(true));
+    }
+}
